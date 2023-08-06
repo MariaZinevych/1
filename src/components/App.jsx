303c9bb3d9c00d237b42495d4c5a524fc8a961cf
MariaZinevych <123274407+MariaZinevych@users.noreply.github.com>
2023-08-07 01:01:46 +0200

    create task1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,25 @@
+import { Profiles } from './Profile/Profile';
+import user from '../user.json';
+
 export const App = () => {
   return (
     <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
+    // style={{
+    //   height: '100vh',
+    //   display: 'flex',
+    //   justifyContent: 'center',
+    //   alignItems: 'center',
+    //   fontSize: 40,
+    //   color: '#010101',
+    // }}
     >
-      React homework template
+      <Profiles
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
     </div>
   );
 };
---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,33 @@
+import { Profile, List } from './Profile.styled';
+
+export const Profiles = ({ username, tag, location, avatar, stats }) => {
+  return (
+    <div>
+      <Profile>
+        <img
+          src="https://cdn-icons-png.flaticon.com/512/1077/1077012.png"
+          alt="User avatar"
+          width={30}
+        />
+        <p>{username}</p>
+        <p>{tag}</p>
+        <p>{location}</p>
+      </Profile>
+
+      <List>
+        <li>
+          <span>Followers</span>
+          <span>{stats.followers}</span>
+        </li>
+        <li>
+          <span>Views</span>
+          <span>{stats.views}</span>
+        </li>
+        <li>
+          <span>Likes</span>
+          <span>{stats.likes}</span>
+        </li>
+      </List>
+    </div>
+  );
+};
